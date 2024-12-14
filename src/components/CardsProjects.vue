88e3a04eb7bc965df240fb1fab75218984049c0f
Dylane <tanolamene@gmail.com>
2024-12-13 22:42:10 -0400

    Mise à jour des variables de typographie et réorganisation des projets dans CardsProjects.vue

--- src/components/CardsProjects.vue
+++ src/components/CardsProjects.vue
@@ -13,38 +13,38 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import figmaCourseChatbot from '../assets/img/figmaCourseChatbot.png'
-import kyotoSite from '../assets/img/KyotoSite.png'
+// import figmaCourseChatbot from '../assets/img/figmaCourseSite.png'
+import Calculatrice from '../assets/img/calculatrice.png'
 import figmaCourseSite from '../assets/img/figmaCourseSite.png'
 import diane from '../assets/img/diane.png'
 import CardProject from './CardProject.vue'
 const projects = ref([
-  {
+{
     id: 1,
-    name: 'Figma course',
-    link: 'https://figma-course-pi.vercel.app/',
-    img: figmaCourseChatbot,
-    categorie: 'Integration',
-  },
-  {
-    id: 2,
-    name: 'Guide Kyoto',
-    link: 'https://guide-kyoto.vercel.app/',
-    img: kyotoSite,
-    categorie: 'Website',
-  },
-  {
-    id: 3,
     name: 'Figma Course',
     link: 'https://www.figma.com/design/qZBIuCbbhoCynV8NzHtxMc/Figma-Courses---Site?node-id=0-1&node-type=canvas&t=JVGCaL3sxRtG1a3x-0',
     img: figmaCourseSite,
     categorie: 'Design Figma',
   },
   {
-    id: 4,
+    id: 2,
+    name: 'Figma course',
+    link: 'https://figma-course-pi.vercel.app/',
+    img: figmaCourseSite,
+    categorie: 'Integration',
+  },
+  {
+    id: 3,
     name: 'Diane',
     link: 'https://diane-sale.vercel.app/',
     img: diane,
+    categorie: 'Website',
+  },
+  {
+    id: 4,
+    name: 'Calculatrice',
+    link: 'https://calculatrice-vanilla.vercel.app/',
+    img: Calculatrice,
     categorie: 'Website',
   },
 ])
